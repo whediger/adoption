d485a0bb96be15cdef4e9b8a8637d0a3b53e9e6e
Michael Gaffney <michaelgaffney@gMichaelGaffney.local>
2021-06-28 13:49:35 -0500

    adding patch

--- src/main/java/org/pupperpals/adoption/repository/PupperRepository.java
+++ src/main/java/org/pupperpals/adoption/repository/PupperRepository.java
@@ -5,10 +5,12 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.transaction.annotation.Transactional;
 
 
 public interface PupperRepository extends JpaRepository<Pupper, Long> {
 
+    @Transactional
     @Modifying
     @Query("Update Pupper p set p.name = :name where p.id = :id")
     void updatePupperName(@Param("name") String name, @Param("id") Long id);
